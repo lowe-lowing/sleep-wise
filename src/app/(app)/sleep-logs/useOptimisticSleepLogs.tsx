8468ad5e6ff9564b44f5c70604271861adc96fff
Lowe Löwing <76205967+lowe-lowing@users.noreply.github.com>
2024-10-19 21:33:14 +0200

    time formating

--- src/app/(app)/sleep-logs/useOptimisticSleepLogs.tsx
+++ src/app/(app)/sleep-logs/useOptimisticSleepLogs.tsx
@@ -1,27 +1,24 @@
-
-import { type SleepLog, type CompleteSleepLog } from "@/lib/db/schema/sleepLogs";
+import {
+  type SleepLog,
+  type CompleteSleepLog,
+} from "@/lib/db/schema/sleepLogs";
 import { OptimisticAction } from "@/lib/utils";
 import { useOptimistic } from "react";
 
 export type TAddOptimistic = (action: OptimisticAction<SleepLog>) => void;
 
-export const useOptimisticSleepLogs = (
-  sleepLogs: CompleteSleepLog[],
-  
-) => {
+export const useOptimisticSleepLogs = (sleepLogs: CompleteSleepLog[]) => {
   const [optimisticSleepLogs, addOptimisticSleepLog] = useOptimistic(
     sleepLogs,
     (
       currentState: CompleteSleepLog[],
-      action: OptimisticAction<SleepLog>,
+      action: OptimisticAction<SleepLog>
     ): CompleteSleepLog[] => {
       const { data } = action;
 
-      
-
       const optimisticSleepLog = {
         ...data,
-        
+
         id: "optimistic",
       };
 
@@ -29,19 +26,22 @@ export const useOptimisticSleepLogs = (
         case "create":
           return currentState.length === 0
             ? [optimisticSleepLog]
-            : [...currentState, optimisticSleepLog];
+            : [...currentState, optimisticSleepLog].sort(
+                (a: SleepLog, b: SleepLog) =>
+                  b.date.getTime() - a.date.getTime()
+              );
         case "update":
           return currentState.map((item) =>
-            item.id === data.id ? { ...item, ...optimisticSleepLog } : item,
+            item.id === data.id ? { ...item, ...optimisticSleepLog } : item
           );
         case "delete":
           return currentState.map((item) =>
-            item.id === data.id ? { ...item, id: "delete" } : item,
+            item.id === data.id ? { ...item, id: "delete" } : item
           );
         default:
           return currentState;
       }
-    },
+    }
   );
 
   return { addOptimisticSleepLog, optimisticSleepLogs };
